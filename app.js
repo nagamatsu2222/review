09cc9b9632c1ba37cc89f4739c840f8f6690234e
Miyu Nagamatsu <miymiy0418@gmail.com>
2023-08-04 14:48:33 +0900

    基本ファイル

---
+++ app.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.set('view engine', 'ejs');
+
+const mysql = require('mysql2');
+const { info } = require('console');
+
+const con = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'root',
+  database: 'express_db'
+});
+
+// cssファイルの取得
+app.use(express.static('assets'));
+
+// mysqlからデータを持ってくる
+app.get('/', (req, res) => {
+  const sql = "select * from questionnaire";
+  con.query(sql, function (err, result, fields) {
+    if (err) throw err;
+    res.render('index', {
+      users: result
+    });
+  });
+});
+
+app.post('/post', (req, res) => {
+  const sql = "INSERT INTO questionnaire SET ?"
+  con.query(sql, req.body, function (err, result, fields) {
+    console.log(req.body)
+    if (err) throw err;
+    console.log(result);
+    res.sendFile(path.join(__dirname, "./", "html", "thanks.html"));
+  });
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
